20ad678f4c28adae382ed47e8fbdeab86a042b43
Daniel <dstolz@umd.edu>
2026-03-07 12:28:45 -0500

    added vprintfs

--- cl/@cl_AppetitiveDetection_GUI_B/eval_rwdelay_training_mode.m
+++ cl/@cl_AppetitiveDetection_GUI_B/eval_rwdelay_training_mode.m
@@ -7,12 +7,14 @@ global RUNTIME % TO DO: FIND A BETTER WAY TO ACCESS RUNTIME.HELPER
 
 
 try
-    
+
     if event.Value == 1
         % launch the training mode GUI
         if ~isempty(obj.h_RWDelayTrainingGUI) && isvalid(obj.h_RWDelayTrainingGUI) % if the GUI doesn't exist or has been deleted, create it
+            vprintf(2,'locating Response Window Delay Training GUI')
             uifigure(obj.h_RWDelayTrainingGUI.UIFigure); % bring to front if it already exists
         else
+            vprintf(2,'Launching Response Window Delay Training GUI')
             h = ProgressiveTrainingGUI(Parameter, ...
                 'MinValue', 100, ...
                 'MaxValue', 4000, ...
@@ -25,35 +27,64 @@ try
 
             obj.hl_RWDelayTrainingGUI = addlistener(RUNTIME.HELPER,'NewData',@(src,event) update_rwdelay_training_mode(src,event,h));
             obj.h_RWDelayTrainingGUI = h;
+            
         end
+
+        % disable the parameter control in the main GUI while training mode is active
+        obj.h_RWDelayParameterControl.h_uiobj.Enable = 'off';
     else
+        vprintf(2,'Closing Response Window Delay Training GUI')
+
         % close the training mode GUI if it's open
         delete(obj.h_RWDelayTrainingGUI);
+
+        % re-enable the parameter control in the main GUI
+        obj.h_RWDelayParameterControl.h_uiobj.Enable = 'on';
     end
 
 catch e
     vprintf(0,1,'Error evaluating Response Window Delay Training Mode: %s',getReport(e,'basic'))
+    obj.h_RWDelayParameterControl.h_uiobj.Enable = 'on'; % ensure the parameter control is re-enabled if there's an error
 end
 
 end
 
 function update_rwdelay_training_mode(src,event,h)
-    % This function is called whenever new trial data is available, and updates the training mode parameters
-    % based on the most recent trial data.
+% This function is called whenever new trial data is available, and updates the training mode parameters
+% based on the most recent trial data.
 global RUNTIME
 
 if isempty(h) || ~isvalid(h), return; end
 
-RC = epsych.BitMask.decodeResponseCodes(RUNTIME.TRIALS.DATA(end).RespCode);
+RC = epsych.BitMask.decode(RUNTIME.TRIALS.DATA(end).RespCode);
 if RC.Hit
-    s = "down";
-elseif RC.Miss
     s = "up";
+elseif RC.Miss
+    s = "down";
 else
     return % only update parameters on Hit or Miss trials
 end
 
 vprintf(3,'Updating Response Window Delay Training Mode parameters based on trial outcome: %s',s)
-h.updateParameter(s); % this will update the training parameters based on the trial outcome (Hit/Miss)
+newValue = h.updateParameter(s); % this will update the training parameters based on the trial outcome (Hit/Miss)
+
+P = h.Parameter;
+
+% CURRENTLY ONLY WORKS FOR SINGLE SUBJECT
+if P.Parent.Type == "Software"
+    curValStr = P.ValueStr;
+    vprintf(3,'Updated parameter "%s": %s -> %s',P.Name,curValStr,P.ValueStr)
+end
+
+T = RUNTIME.TRIALS.trials;
+loc = RUNTIME.TRIALS.writeParamIdx;
+
+if isfield(loc,P.validName)
+    [T{:,loc.(P.validName)}] = deal(newValue);
+end
+
+
+RUNTIME.TRIALS.trials = T;
+
 
 end

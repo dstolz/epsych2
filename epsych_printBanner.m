f6c419ec47ba2aa8cfb93d89f3a0949a68357311
Daniel <dstolz@umd.edu>
2026-03-18 14:30:32 -0400

    feat(PrintBanner): Add latest commit information to banner output

--- epsych_printBanner.m
+++ epsych_printBanner.m
@@ -16,6 +16,7 @@ E = EPsychInfo;
 
 cm = cellstr(m);
 cm{end} = sprintf('%s\nv%s <a href="%s">%s</a>',cm{end},E.Version,E.LicenseURL,E.Copyright);
+cm{end+1} = sprintf('Latest commit: %s; %s',E.commitTimestamp,E.chksum(1:7));
 lnk = E.RepositoryURL;
 cm{end+1} = sprintf('Repository: <a href="%s">%s</a>',lnk,lnk);
 %cm{end+1} = '-> <a href="matlab: ep_LaunchPad">ep_LaunchPad</a>  ... Launch panel for EPsych utilities';

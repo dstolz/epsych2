56a72a7eb9775b4a637cb528268b36ab2a581f71
Daniel <dstolz@umd.edu>
2026-03-25 14:25:23 -0400

    fix(PsychTimerStart, buildUI): update menu item variable name for launching behavior GUI

--- obj/+epsych/@RunExpt/PsychTimerStart.m
+++ obj/+epsych/@RunExpt/PsychTimerStart.m
@@ -17,13 +17,11 @@ if isempty(self.FUNCS.BoxFig)
 else
     try
         feval(self.FUNCS.BoxFig, self.RUNTIME);
-        set(self.H.mnu_LaunchGUI,'Enable','on')
     catch me
         s = self.FUNCS.BoxFig;
         if ~ischar(s), s = func2str(s); end
         vprintf(0,1,me)
         a = repmat('*',1,50);
-        set(self.H.mnu_LaunchGUI,'Enable','off')
         vprintf(0,1,'%s\nFailed to launch behavior performance GUI: %s\n%s',a,s,a)
     end
 end

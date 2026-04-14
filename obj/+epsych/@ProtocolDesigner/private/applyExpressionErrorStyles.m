0ff6b82371b5731e49498c6e8ce94a5704102ea6
Daniel <dstolz@umd.edu>
2026-04-14 14:45:28 -0400

    feat: Add 'Pair' option to dropdown and handle new case in error styling

--- obj/+epsych/@ProtocolDesigner/private/applyExpressionErrorStyles.m
+++ obj/+epsych/@ProtocolDesigner/private/applyExpressionErrorStyles.m
@@ -14,6 +14,8 @@ function applyExpressionErrorStyles(obj)
             localApplyCategoricalRowStyles_(obj, localGetTableColumn_(obj.TableParams.Data, 1));
         case 'module'
             localApplyCategoricalRowStyles_(obj, localGetTableColumn_(obj.TableParams.Data, 2));
+        case 'pair'
+            localApplyCategoricalRowStyles_(obj, localGetTableColumn_(obj.TableParams.Data, 6));
         otherwise
             localApplyTypeRowStyles_(obj);
     end

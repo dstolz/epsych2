810745ebe697e6c3b16ed3827698e3032b5bcfd8
Daniel <dstolz@umd.edu>
2026-05-05 15:07:55 -0400

    .

--- epsych_printBanner.m
+++ epsych_printBanner.m
@@ -24,11 +24,10 @@ cm{end+1} = sprintf('Latest commit: %s; %s; <a href="%s">Commit History Overview
 cm{end+1} = sprintf('<a href="%s">Visit Documentation</a>',E.DocumentationURL);
 lnk = E.RepositoryURL;
 cm{end+1} = sprintf('Repository: <a href="%s">%s</a>',lnk,lnk);
-cm{end+1} = '--> <a href="matlab: epsych.ProtocolDesigner">epsych.ProtocolDesigner</a>    ... Define parameters for experiments';
-cm{end+1} = '--> <a href="matlab: epsych.BitMask.GUI">epsych.BitMask.GUI</a>         ... Bitmask table generator for behavioral experiments';
-%cm{end+1} = '--> <a href="matlab: epsych.CalibrationUtil">epsych.CalibrationUtil</a>   ... Sound calibration utility';
-cm{end+1} = '--> <a href="matlab: epsych.RunExpt">epsych.RunExpt</a>             ... Behavior/Electrophysiology';
 cm{end+1} = '--> <a href="matlab: stimgen.StimPlayer">stimgen.StimPlayer</a>         ... Sound stimulus player';
+%cm{end+1} = '--> <a href="matlab: epsych.BitMask.GUI">epsych.BitMask.GUI</a>         ... Bitmask table generator for behavioral experiments';
+cm{end+1} = '--> <a href="matlab: epsych.ProtocolDesigner">epsych.ProtocolDesigner</a>    ... Define parameters for experiments';
+cm{end+1} = '--> <a href="matlab: epsych.RunExpt">epsych.RunExpt</a>             ... Behavior/Electrophysiology';
 
 fprintf('\n')
 for i = 1:length(cm), fprintf('%s\n',cm{i}); end
